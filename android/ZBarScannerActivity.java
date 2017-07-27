c8c915d5b97221cea8c27115f0e742d9b5cd136e
KajolChaudhary <kajol@samcomtechnobrains.com>
2017-07-27 16:52:00 +0530

    import toast

--- android/ZBarScannerActivity.java
+++ android/ZBarScannerActivity.java
@@ -32,6 +32,7 @@ import android.view.Surface;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collection;
